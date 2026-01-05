3d8411f75cd2683ed6246be6d47fb38f5276fa32
Hrafnir <eirik.gaarde@gmail.com>
2026-01-05 14:18:56 +0100

    Update data.js to version 5 with new milestones

--- js/data.js
+++ js/data.js
@@ -1,107 +1,100 @@
-/* Version: #3 */
+/* Version: #5 */
 /* 
     DATA KONFIGURASJON FOR DYPTIDS-REISEN
     -------------------------------------
-    Denne filen inneholder en liste over geologiske tidsaldre (milepæler).
-    Hvert objekt definerer:
-    - start/end: Tidsintervall i millioner år før nåtid (Ma).
-    - title: Navnet på perioden.
-    - color: Hex-fargekode for 3D-kloden (0xRrGgBb).
-    - desc: Tekstlig beskrivelse av hva som skjer.
-    - img: Navn på bilde/plassholder tekst.
+    Basert på "Dyptidspedagogikk: En kronostratigrafisk analyse".
 */
 
 const MILESTONES = [
     { 
         start: 4600, 
         end: 4000, 
-        title: "Hadeikum", 
-        color: 0xff3300, // Rød (Magma)
-        desc: "Solsystemet dannes. Jorden er en vulkansk, smeltet klode. Månen dannes etter kollisjon med Theia. De første havene kondenserer mot slutten av perioden.", 
-        img: "hadeikum_magma.jpg" 
+        title: "Hadeikum: Smeltedigelen", 
+        color: 0xff3300, 
+        desc: "Jorden dannes av kosmisk støv og stein. 4,53 Ga kolliderer planeten Theia med Jorden; restene danner Månen. En glødende magma-klode kjøles sakte ned, og de første havene kondenserer fra en tykk, trykkende atmosfære.", 
+        img: "hadean_magma.jpg" 
     },
     { 
         start: 4000, 
         end: 3600, 
-        title: "Eoarkeikum", 
-        color: 0x0066cc, // Dyp Blå (Vannverden)
-        desc: "Livets begynnelse? Jordskorpen størkner. En global vannverden oppstår. De aller første encellede organismene (prokaryoter) dukker opp rundt hydrotermiske skorsteiner.", 
-        img: "eoarkeikum_water.jpg" 
+        title: "Eoarkeikum: Vannverdenen", 
+        color: 0x0066cc, 
+        desc: "En global vannverden med kun små vulkanske øyer. Livet oppstår (Abiogenese) i hydrotermiske skorsteiner. Atmosfæren er oksygenfri og oransje av metan. De eldste bergartene (Acasta Gneiss) bevares.", 
+        img: "eoarchean_water.jpg" 
     },
     { 
         start: 3600, 
         end: 3200, 
-        title: "Paleoarkeikum", 
-        color: 0x0088aa, // Blågrønn
-        desc: "De første stromatolittene dannes. Anoksygenisk fotosyntese utvikles (lager ikke oksygen ennå). Mikrobielle matter dominerer i grunt vann.", 
-        img: "stromatolitter.jpg" 
+        title: "Paleoarkeikum: Stromatolitter", 
+        color: 0x0088aa, 
+        desc: "De første makrofossilene: Stromatolitter bygger rev i grunt vann. Anoksygenisk fotosyntese oppstår – bakterier utnytter sollys men lager svovel, ikke oksygen. Mulig dannelse av ur-kontinentet Vaalbara.", 
+        img: "paleoarchean_stromatolites.jpg" 
     },
     { 
         start: 3200, 
         end: 2800, 
-        title: "Mesoarkeikum", 
-        color: 0x009999, // Turkis (Cyanobakterier start)
-        desc: "De første superkontinentene (Vaalbara) begynner å ta form. Cyanobakterier utvikler seg og begynner sakte å produsere oksygen som et avfallsstoff.", 
-        img: "cyanobakterier.jpg" 
+        title: "Mesoarkeikum: Is-Paradokset", 
+        color: 0x009999, 
+        desc: "Kontinentalsoklene vokser og gir livsrom. Cyanobakterier utvikler evnen til å splitte vann og lage Oksygen ($O_2$). Pongola-istiden (2,9 Ga) viser at klimaet svinger selv i en drivhusverden.", 
+        img: "mesoarchean_ice.jpg" 
     },
     { 
         start: 2800, 
         end: 2400, 
-        title: "Neoarkeikum", 
-        color: 0x55aa55, // Rust-grønn
-        desc: "Oksygenproduksjonen øker, men absorberes umiddelbart av jern i havet, noe som skaper 'Banded Iron Formations'. Tektonisk aktivitet øker.", 
-        img: "iron_formations.jpg" 
+        title: "Neoarkeikum: Oksygenkatastrofen", 
+        color: 0x55aa55, 
+        desc: "Havet 'ruster': Oksygen reagerer med jern og danner Båndet Jernmalm (BIF). Dette leder til The Great Oxidation Event (GOE). Oksygenet er giftig for det meste av liv, men åpner for effektiv aerob energi.", 
+        img: "goe_red_ocean.jpg" 
     },
     { 
         start: 2400, 
         end: 2000, 
-        title: "Paleoproterozoikum", 
-        color: 0xffffff, // Hvit (Snøballjord)
-        desc: "The Great Oxidation Event (GOE)! Oksygen dreper anaerobt liv. Metan i atmosfæren brytes ned, noe som utløser Huronian istid – Jorden blir en 'Snøball'. Første eukaryote celler oppstår.", 
+        title: "Paleoproterozoikum: Snøballjord", 
+        color: 0xffffff, 
+        desc: "Huronian-istiden gjør jorden til en hvit 'Snøball' etter at metan forsvinner. Vredefort-asteroiden treffer. Det største biologiske spranget skjer: Endosymbiose skaper den Eukaryote cellen (med kjerne).", 
         img: "snowball_earth.jpg" 
     },
     { 
         start: 2000, 
         end: 1600, 
-        title: "Mesoproterozoikum (Columbia)", 
-        color: 0xccaa00, // Gul-brun (Landmasse/Kjedelig)
-        desc: "Superkontinentet Columbia dannes. Perioden kalles ofte 'Den kjedelige milliarden' på grunn av geologisk stabilitet og langsom evolusjon. Eukaryoter sprer seg.", 
-        img: "columbia_continent.jpg" 
+        title: "Mesoproterozoikum I: Columbia", 
+        color: 0xccaa00, 
+        desc: "Superkontinentet Columbia dannes. 'Den kjedelige milliarden' preges av stabilitet. Havene er lagdelte (Canfield Ocean) med giftig svovel i dypet, noe som bremser utviklingen av avansert liv.", 
+        img: "columbia_supercontinent.jpg" 
     },
     { 
         start: 1600, 
         end: 1200, 
-        title: "Mesoproterozoikum (Bangiomorpha)", 
-        color: 0xaabb00, // Lys grønn
-        desc: "Flercellet liv oppstår (f.eks. rødalgen Bangiomorpha). Kloroplaster utvikles gjennom endosymbiose, som gir alger evnen til fotosyntese.", 
-        img: "multicellular_life.jpg" 
+        title: "Mesoproterozoikum II: Flercellethet", 
+        color: 0xaabb00, 
+        desc: "Rødalgen Bangiomorpha (1,2 Ga) er det første beviset på spesialisert flercellet liv. Kloroplaster oppstår ved at en celle 'spiser' en cyanobakterie uten å fordøye den – planteriket fødes.", 
+        img: "bangiomorpha_algae.jpg" 
     },
     { 
         start: 1200, 
         end: 800,  
-        title: "Neoproterozoikum (Rodinia)", 
-        color: 0x996633, // Brun (Rodinia)
-        desc: "Superkontinentet Rodinia samles. En revolusjonær oppfinnelse skjer: Seksuell reproduksjon (kjønn). Dette akselererer evolusjonstakten betraktelig.", 
-        img: "rodinia.jpg" 
+        title: "Neoproterozoikum I: Rodinia & Kjønn", 
+        color: 0x996633, 
+        desc: "Superkontinentet Rodinia samles. Oppfinnelsen av seksuell reproduksjon (kjønn) akselererer evolusjonen drastisk ved å stokke genene. Et evolusjonært våpenkappløp (pigger og panser) begynner.", 
+        img: "rodinia_sex.jpg" 
     },
     { 
         start: 800,  
         end: 400,  
-        title: "Kambrium & Tidlig Liv", 
-        color: 0x00dddd, // Is til Liv (Overgang)
-        desc: "Kryogenisk istid (Snøballjord igjen) etterfølges av Ediacara-faunaen og den Kambriske eksplosjonen. Livet tar sine første steg opp på land.", 
+        title: "Neoproterozoikum II: Eksplosjon", 
+        color: 0x00dddd, 
+        desc: "Etter en ny Snøball-jord (Cryogenium) kommer Ediacara-faunaen (bløte dyr). Så smeller det: Den Kambriske Eksplosjon (541 Ma). Dyr får øyne, tenner og skall. Livet krabber opp på land i Silur.", 
         img: "cambrian_explosion.jpg" 
     },
     { 
         start: 400,  
         end: 0,    
-        title: "Fanerozoikum (Nåtid)", 
-        color: 0x228822, // Frodig Grønn (Jorden i dag)
-        desc: "Dinosaurenes tidsalder, fremveksten av pattedyr, og til slutt: Mennesket. Oksygennivået stabiliseres rundt 21%.", 
-        img: "modern_earth.jpg" 
+        title: "Fanerozoikum: Land & Liv", 
+        color: 0x228822, 
+        desc: "Karbonskogene, Pangea, Dinosaurenes tidsalder og til slutt Pattedyr. Mennesket (Homo Sapiens) dukker opp helt i siste sekund av denne 4,6 milliarder år lange reisen.", 
+        img: "phanerozoic_land.jpg" 
     }
 ];
 
-// Logg til konsollen for å bekrefte at data er lastet
-console.log("System: Data-modul lastet. Antall milepæler:", MILESTONES.length);
-/* Version: #3 */
+console.log("System: Dyptidspedagogikk-data lastet. Antall faser:", MILESTONES.length);
+/* Version: #5 */
